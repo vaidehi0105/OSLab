5f8961f6331ce704805d45bdf32e39a0f32000a3
Aditya Mitra <adityaarghya0@gmail.com>
2022-04-06 09:40:49 +0530

    Update lab_backend.sh

--- process_management_backend/lab_backend.sh
+++ process_management_backend/lab_backend.sh
@@ -1,6 +1,7 @@
 sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get install curl
+sudo apt-get install tree
 sudo apt-get install python3
 sudo apt-get install python3-pip
 sudo apt-get install python3-flask
