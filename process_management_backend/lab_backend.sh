a88272cafa3e3ff949f64a835c71d654694c06b2
Aditya Mitra <adityaarghya0@gmail.com>
2022-04-06 09:52:21 +0530

    Update lab_backend.sh

--- process_management_backend/lab_backend.sh
+++ process_management_backend/lab_backend.sh
@@ -1,3 +1,4 @@
+rm lab_backend*
 sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get install curl
@@ -8,4 +9,3 @@ sudo apt-get install python3-flask
 pip3 install flask
 pip3 install flask-cors
 curl https://AdityaMitra5102.github.io/OSLab/process_management_backend/flaskapp.py | python3
-rm lab_backend.sh
